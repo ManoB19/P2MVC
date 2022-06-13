e30926afee21fffcdfcbe0e2dd3a5e3f865d2a54
MANOB <ebersonbaruc123@gmail.com>
2022-06-13 13:43:00 -0300

    atualizei o init do application

--- application/__init__.py
+++ application/__init__.py
@@ -4,3 +4,8 @@ import os
 app = Flask(__name__,
             static_folder=os.path.abspath("application/view/static"),
             template_folder=os.path.abspath("application/view/templates"))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+app.run()
