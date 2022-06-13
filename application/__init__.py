4425cdb8bdfd88393f217bb0f9559fb1215c221a
MANOB <ebersonbaruc123@gmail.com>
2022-06-13 15:18:55 -0300

    Att: imagem

--- application/__init__.py
+++ application/__init__.py
@@ -5,6 +5,8 @@ app = Flask(__name__,
             static_folder=os.path.abspath("application/view/static"),
             template_folder=os.path.abspath("application/view/templates"))
 
+from application.controller import home_controller
+
 if __name__ == '__main__':
     app.run(debug=True)
 
